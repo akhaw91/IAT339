2ba9d8af219d6de094c33bbd4d15a25d49320e8f
Faiflow <theow@sfu.ca>
2015-03-10 00:54:38 -0700

    creations of the featureprod.js

---
+++ js/featureprod.js
@@ -0,0 +1,16 @@
+
+$(document).ready( function() {
+
+	var width = $(window).width();
+
+	if (width < 480) {
+		$("#breadcrumbs").hide();
+		$("#prodimg").css({"display":"inline-block", "margin-left":"0"});
+		$("#info").css({"position":"relative"});
+
+}
+
+
+
+
+});
