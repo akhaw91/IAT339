6ea6e8f50ec992b56c5fecf8984dfa7816925729
Faiflow <theow@sfu.ca>
2015-03-09 23:22:05 -0700

    applied moble settings to index only. applied Jqueary applications and sliding menu. adjusted index nav settings

---
+++ js/hamburger2.js
@@ -0,0 +1,24 @@
+
+$(document).ready( function() {
+
+	var width = $(window).width();
+
+	if (width < 480) {
+		$("#logoWord").css({"padding-left":"8rem", "padding-top":"2rem" });
+		$("#navToggle").css("display","block"); 
+		$("nav").hide();		
+		$("#logo").hide();
+		$("#message").hide();
+
+
+
+	}
+
+	$("#navToggle").click( function() { 
+		$("nav").slideToggle(500);
+	});
+
+
+
+
+});
